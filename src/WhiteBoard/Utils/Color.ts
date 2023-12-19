88414313af10f1c16df9a79bb9c96d41e4253a2f
EvilAlliance <chialanza17@gmail.com>
2023-12-19 10:06:11 -0300

    ColorRGBA for Animation

--- src/WhiteBoard/Utils/Color.ts
+++ src/WhiteBoard/Utils/Color.ts
@@ -3,3 +3,34 @@ import Color from './../Constantes/Color.json';
 export function CanvasParseColor(x: string): string {
     return Color[x as keyof typeof Color] ?? x;
 }
+
+export type ColorRGBA = [number, number, number, number];
+
+export function ColorRGBAToParse(x: string): ColorRGBA {
+    if (x[0] === '#') return hexToColorRGBA(x);
+    if (x[0] === 'r') return rgbToColorRGBA(x);
+    return hexToColorRGBA(Color[x as keyof typeof Color]);
+}
+
+function hexToColorRGBA(hex: string): ColorRGBA {
+    const r = parseInt(hex.slice(1, 3), 16) || 0;
+    const b = parseInt(hex.slice(3, 5), 16) || 0;
+    const g = parseInt(hex.slice(5, 7), 16) || 0;
+    const a = parseInt(hex.slice(7, 9), 16) || 1;
+    return [r, b, g, a];
+}
+
+function rgbToColorRGBA(rgb: string): ColorRGBA {
+    const x = rgb.slice(4, rgb.length - 1);
+    const a = x.split(',');
+    return [
+        parseInt(a[0]) || 0,
+        parseInt(a[1]) || 0,
+        parseInt(a[2]) || 0,
+        parseInt(a[3]) || 1,
+    ];
+}
+
+export function ColorRGBAParse(x: ColorRGBA): string {
+    return `rgb(${x[0]},${x[1]},${x[2]},${x[3]})`;
+}
