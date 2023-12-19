23a8c9899b5c24a0537d5ee92ee661329c78aebd
EvilAlliance <chialanza17@gmail.com>
2023-12-19 09:48:59 -0300

    Light change

--- src/WhiteBoard/index.ts
+++ src/WhiteBoard/index.ts
@@ -1,1 +1,14 @@
+/*
 export * from './Canvas';
+export * from './Observable';
+export * from './CommonMethod';
+export * from './Utils/Color';
+export * from './Object/Rect';
+export * from './Object/CanvasObject';
+export * from './Object/CanvasObjectContainer';
+export * from './Animate/Transition';
+export * from './Animate/Animate';
+
+import ColorJSON from './Constantes/Color.json';
+export const Color = ColorJSON;
+*/
