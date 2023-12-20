b27aa2cdcaf79d546f0050dee1caad0caed38c32
EvilAlliance <chialanza17@gmail.com>
2023-12-20 19:46:52 -0300

    Change namimg in Vector

--- src/WhiteBoard/GeoSpace/Vector.ts
+++ src/WhiteBoard/GeoSpace/Vector.ts
@@ -4,8 +4,8 @@ export class Vector {
     public x: number;
     public y: number;
 
-    constructor(p1: Point, p2: Point) {
-        this.x = p2.x - p1.x;
-        this.y = p2.y - p1.y;
+    constructor(initial: Point, final: Point) {
+        this.x = final.x - initial.x;
+        this.y = final.y - initial.y;
     }
 }
