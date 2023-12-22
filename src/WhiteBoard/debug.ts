e833db4da0a53e3e20b09b77e8b3ad8d3708ea60
EvilAlliance <chialanza17@gmail.com>
2023-12-22 16:22:11 -0300

    debug

---
+++ src/WhiteBoard/debug.ts
@@ -0,0 +1,27 @@
+import { Canvas } from "./Canvas";
+import { Point } from "./GeoSpace/Point";
+import { Path } from "./Object/Path";
+import { CanvasParseColor, Color } from "./Utils/Color";
+
+export function drawDot(canvas: Canvas, p: Point, color: Color) {
+    const { ctx } = canvas;
+    ctx.strokeStyle = CanvasParseColor(color);
+    ctx.fillStyle = CanvasParseColor(color);
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, 1, 0 * Math.PI, 1.5 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+export function trailDots(canvas: Canvas, path: Path, color: Color) {
+    const p = path.Path;
+    const { ctx } = canvas;
+    ctx.strokeStyle = CanvasParseColor(color);
+    ctx.fillStyle = CanvasParseColor(color);
+    for (const dot of p) {
+        ctx.beginPath();
+        ctx.arc(dot.x, dot.y, 1, 0 * Math.PI, 1.5 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+}
