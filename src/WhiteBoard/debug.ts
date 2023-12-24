d78170e9e4f77c57a019ae2598c9396267d0c08e
EvilAlliance <chialanza17@gmail.com>
2023-12-24 17:05:50 -0300

    save and resotore context for debug

--- src/WhiteBoard/debug.ts
+++ src/WhiteBoard/debug.ts
@@ -5,17 +5,20 @@ import { CanvasParseColor, Color } from "./Utils/Color";
 
 export function drawDot(canvas: Canvas, p: Point, color: Color) {
     const { ctx } = canvas;
+    ctx.save();
     ctx.strokeStyle = CanvasParseColor(color);
     ctx.fillStyle = CanvasParseColor(color);
     ctx.beginPath();
     ctx.arc(p.x, p.y, 1, 0 * Math.PI, 1.5 * Math.PI);
     ctx.stroke();
     ctx.fill();
+    ctx.restore();
 }
 
 export function trailDots(canvas: Canvas, path: Path, color: Color) {
     const p = path.Path;
     const { ctx } = canvas;
+    ctx.save();
     ctx.strokeStyle = CanvasParseColor(color);
     ctx.fillStyle = CanvasParseColor(color);
     for (const dot of p) {
@@ -24,4 +27,5 @@ export function trailDots(canvas: Canvas, path: Path, color: Color) {
         ctx.stroke();
         ctx.fill();
     }
+    ctx.restore();
 }
