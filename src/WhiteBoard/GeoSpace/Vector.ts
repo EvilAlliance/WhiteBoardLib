dbea639b20c59dfa38a3b76431ed52acd318349e
EvilAlliance <chialanza17@gmail.com>
2023-12-25 21:00:54 -0300

    VectorRotate and VectorAng

--- src/WhiteBoard/GeoSpace/Vector.ts
+++ src/WhiteBoard/GeoSpace/Vector.ts
@@ -14,7 +14,22 @@ export function VectorMod(Vec: Vector): number {
     return Math.hypot(Vec.x, Vec.y);
 }
 
+export function VectorAng(vec: Vector): number {
+    const x = Math.sign(vec.x) == -1 ? Math.PI : 0
+    return Math.atan(vec.y / vec.x) + x
+}
+
 export function VectorTranslatePoint(vec: Vector, p: Point) {
     p.x += vec.x;
     p.y += vec.y;
+}
+
+/**
+ * x rad
+ * */
+export function VectorRotate(vec: Vector, x: number) {
+    const mod = VectorMod(vec);
+    const ang = VectorAng(vec) + x;
+    vec.x = mod * Math.cos(ang);
+    vec.y = mod * Math.sin(ang);
 }
