ed8732ab1db72242119000cc4382add3a02a0f9d
EvilAlliance <chialanza17@gmail.com>
2024-02-15 16:47:24 -0300

    All Observable into a class

--- src/WhiteBoard/Observable.ts
+++ src/WhiteBoard/Observable.ts
@@ -1,44 +1,44 @@
 export type TEventCallback<T = any> = (options: T) => void;
 
-export function on<T, K extends keyof T>(obj: { eventListener: Record<keyof T, TEventCallback[]> }, evtName: K, handler: TEventCallback<T[K]>) {
-    if (!obj.eventListener[evtName]) {
-        obj.eventListener[evtName] = [];
+export class Observable<T = Object>{
+    eventListener: Record<keyof T, TEventCallback[]> = {} as Record<keyof T, TEventCallback[]>;
+
+    on<K extends keyof T = keyof T>(evtName: K, handler: TEventCallback<T[K]>): VoidFunction {
+        this.eventListener[evtName].push(handler);
+        return () => this.off(evtName, handler);
     }
-    obj.eventListener[evtName].push(handler);
-    return () => off(obj, evtName, handler);
-}
 
-export function once<T, K extends keyof T>(obj: { eventListener: Record<keyof T, TEventCallback[]> }, evtName: K, handler: TEventCallback<T[K]>) {
-    const disposer = on(obj, evtName, function onceHandler(...args) {
-        handler.bind(obj)(...args);
-        disposer();
-    });
-    return disposer;
-}
-
-export function removeEventListener<T, K extends keyof T>(obj: { eventListener: Record<keyof T, TEventCallback[]> }, eventName: K, handler?: TEventCallback<T[K]>) {
-    if (!handler) {
-        obj.eventListener[eventName] = [];
-    } else {
-        const eventListener = obj.eventListener[eventName];
-        const index = eventListener.indexOf(handler);
-        index > -1 && eventListener.splice(index, 1);
+    once<K extends keyof T = keyof T>(evtName: K, handler: TEventCallback<T[K]>) {
+        const disposer = this.on(evtName, function onceHandler(...args) {
+            handler(...args);
+            disposer();
+        });
+        return disposer;
     }
-}
-
-export function off<T, K extends keyof T>(obj: { eventListener: Record<keyof T, TEventCallback[]> }, arg0: K, handler: TEventCallback<T[K]>) {
-    if (!arg0) {
-        for (const eventName in obj.eventListener) {
-            removeEventListener(obj, eventName);
+    removeEventListener<K extends keyof T>(eventName: K, handler?: TEventCallback<T[K]>) {
+        if (!handler) {
+            this.eventListener[eventName] = [];
+        } else {
+            const eventListener = this.eventListener[eventName];
+            const index = eventListener.indexOf(handler);
+            index > -1 && eventListener.splice(index, 1);
         }
-    } else {
-        removeEventListener(obj, arg0, handler);
     }
-}
 
-export function fire<T, K extends keyof T>(obj: { eventListener: Record<keyof T, TEventCallback[]> }, eventName: K, options: T[K]) {
-    const events = obj.eventListener[eventName] || [];
-    for (let i = 0; i < events.length; i++) {
-        events[i].bind(obj)(options || {});
+    off<K extends keyof T>(arg0: K, handler: TEventCallback<T[K]>) {
+        if (!arg0) {
+            for (const eventName in this.eventListener) {
+                this.removeEventListener(eventName);
+            }
+        } else {
+            this.removeEventListener(arg0, handler);
+        }
+    }
+
+    fire<K extends keyof T>(eventName: K, options: T[K]) {
+        const events = this.eventListener[eventName] || [];
+        for (let i = 0; i < events.length; i++) {
+            events[i](options || {});
+        }
     }
 }
