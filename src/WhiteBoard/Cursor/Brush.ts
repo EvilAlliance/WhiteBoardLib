e45bf7bbb98f2520ebfe417b93cc4ca40a8a3401
EvilAlliance <chialanza17@gmail.com>
2024-02-16 18:57:49 -0300

    Class Brush all insede the class

--- src/WhiteBoard/Cursor/Brush.ts
+++ src/WhiteBoard/Cursor/Brush.ts
@@ -1,8 +1,8 @@
-import { Canvas, CanvasClear, CanvasRender } from '../Canvas';
+import { Canvas } from '../Canvas';
 import { Point } from '../GeoSpace/Point';
-import { Vector, VectorMod } from '../GeoSpace/Vector';
-import { CtxSetting } from '../Object/BaseObject';
+import { Vector } from '../GeoSpace/Vector';
 import { CanvasObjectContainer } from '../Object/CanvasObjectContainer';
+import { CtxSetting } from '../Object/CtxSetting';
 import { Path } from '../Object/Path';
 import { Color } from '../Utils/Color';
 import { trailDots } from '../debug';
@@ -19,66 +19,56 @@ export class Brush extends BaseBrush<Path> {
     }
 
     mouseDown(canvas: Canvas<this>): Path {
-        return BrushMouseDown(canvas);
+        const p = new Path({
+            ctxSetting: new CtxSetting({
+                strokeWidth: canvas.cursor.width,
+                strokeColor: canvas.cursor.color,
+                lineCap: canvas.cursor.lineCap,
+                globalCompositeOperation: canvas.cursor.globalCompositeOperation,
+            })
+        });
+
+        canvas.Objects.push(new CanvasObjectContainer(p));
+
+        return p;
     }
 
-    mouseMove(canvas: Canvas<this>, e: MouseEvent, obj: Path): void {
-        BrushMouseMove(canvas, e, obj);
-    }
+    mouseMove(canvas: Canvas<this>, e: MouseEvent, path: Path): void {
+        const p = new Point(e.offsetX, e.offsetY);
 
-    mouseUp(canvas: Canvas<this>, e: MouseEvent, obj: Path): void {
-
-    }
-}
-
-export function BrushMouseDown(canvas: Canvas<Brush>): Path {
-    const p = new Path({
-        ctxSetting: new CtxSetting({
-            strokeWidth: canvas.cursor.width,
-            strokeColor: canvas.cursor.color,
-            lineCap: canvas.cursor.lineCap,
-            globalCompositeOperation: canvas.cursor.globalCompositeOperation,
-        })
-    });
-
-    canvas.Objects.push(new CanvasObjectContainer(p));
-
-    return p;
-}
-
-export function BrushMouseMove(canvas: Canvas<Brush>, e: MouseEvent, path: Path) {
-    if (!(canvas.cursor instanceof Brush)) return;
-    const p = new Point(e.offsetX, e.offsetY);
-
-    if (path.Path.length == 0) {
-        path.Path.push(p);
-    } else {
-        const v = new Vector(p, path.Path[path.Path.length - 1]);
-        if (VectorMod(v) >= canvas.cursor.width / 2) {
+        if (path.Path.length == 0) {
             path.Path.push(p);
+        } else {
+            const v = new Vector(p, path.Path[path.Path.length - 1]);
+            if (v.mod() >= canvas.cursor.width / 2) {
+                path.Path.push(p);
+            }
         }
+
+        canvas.clear();
+        canvas.render();
     }
 
-    CanvasClear(canvas);
-    CanvasRender(canvas);
+    //simplify path prototype not used 
+    mouseUp(canvas: Canvas<this>, e: MouseEvent, path: Path): void {
+        return
+        const p = JSON.parse(JSON.stringify(path));
+        let curr = path.Path[0];
+        let i = 1;
+        while (i < path.Path.length - 1) {
+            const nextVec = new Vector(curr, path.Path[i]);
+            const nextNextVec = new Vector(curr, path.Path[i + 1]);
+            const newVec = new Vector(path.Path[i], path.Path[i + 1]);
+            if (Math.abs(Math.atan(nextVec.y / nextVec.x) - Math.atan(nextNextVec.y / nextNextVec.x)) < Math.PI / 90 && Math.abs(Math.atan(newVec.y / newVec.x)) > Math.PI / 90 - Math.PI / 180) {
+                path.Path.splice(i--, 1);
+            }
+            curr = path.Path[i++];
+        }
+
+        canvas.clear();
+        canvas.render();
+        trailDots(canvas.ctx, p, 'Black');
+        trailDots(canvas.ctx, path, 'Bulrush');
+    }
 }
 
-//simplify path prototype not used 
-function BrushMouseUp(canvas: Canvas, path: Path) {
-    const p = JSON.parse(JSON.stringify(path));
-    let curr = path.Path[0];
-    let i = 1;
-    while (i < path.Path.length - 1) {
-        const nextVec = new Vector(curr, path.Path[i]);
-        const nextNextVec = new Vector(curr, path.Path[i + 1]);
-        const newVec = new Vector(path.Path[i], path.Path[i + 1]);
-        if (Math.abs(Math.atan(nextVec.y / nextVec.x) - Math.atan(nextNextVec.y / nextNextVec.x)) < Math.PI / 90 && Math.abs(Math.atan(newVec.y / newVec.x)) > Math.PI / 90 - Math.PI / 180) {
-            path.Path.splice(i--, 1);
-        }
-        curr = path.Path[i++];
-    }
-    CanvasClear(canvas);
-    CanvasRender(canvas);
-    trailDots(canvas.ctx, p, 'Black');
-    trailDots(canvas.ctx, path, 'Bulrush');
-}
