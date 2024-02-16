66d006bb945c8da61d395376584ea174115b6cb7
EvilAlliance <chialanza17@gmail.com>
2024-02-16 15:52:13 -0300

    Class CtxTranformation with their function

---
+++ src/WhiteBoard/Object/CtxTransformation.ts
@@ -0,0 +1,64 @@
+import { Point } from "../GeoSpace/Point";
+import { BoundingBox } from "./BoundingBox";
+
+export const OriginXY = Object.freeze({
+    top: 0,
+    left: 0,
+    center: 0.5,
+    right: 1,
+    bottom: 1,
+});
+
+export class CtxTransformation {
+    originX: 'left' | 'center' | 'right' | number = 'left';
+    originY: 'top' | 'center' | 'bottom' | number = 'top';
+    skewY: number = 0;
+    skewX: number = 0;
+    scaleY: number = 1;
+    scaleX: number = 1;
+    angle: number = 0;
+    constructor(obj?: Partial<CtxTransformation>) {
+        Object.assign(this, obj);
+    }
+
+    setContextTransformation(ctx: CanvasRenderingContext2D, boundingBox: BoundingBox) {
+        const centerPoint = this.getCenterPoint(boundingBox);
+
+        ctx.transform(
+            this.scaleX,
+            this.skewY,
+            this.skewX,
+            this.scaleY,
+            centerPoint.x,
+            centerPoint.y
+        );
+
+        ctx.rotate(this.angle);
+
+        ctx.translate(-centerPoint.x, -centerPoint.y);
+    }
+
+    GetTransformationMatrix(boundingBox: BoundingBox) {
+        const centerPoint = this.getCenterPoint(boundingBox);
+
+        const transformationMat = new DOMMatrix();
+
+        transformationMat.a = this.scaleX;
+        transformationMat.b = this.skewY;
+        transformationMat.c = this.skewX;
+        transformationMat.d = this.scaleY;
+        transformationMat.e = centerPoint.x;
+        transformationMat.f = centerPoint.y;
+
+        transformationMat.rotateSelf(this.angle / Math.PI * 180);
+        transformationMat.translateSelf(-centerPoint.x, -centerPoint.y);
+
+        return transformationMat;
+    }
+
+    getCenterPoint({ tl }: BoundingBox): Point {
+        const x = OriginXY[(this.originX ?? 'left') as keyof typeof OriginXY] ?? this.originX;
+        const y = OriginXY[(this.originY ?? 'top') as keyof typeof OriginXY] ?? this.originY;
+        return new Point(tl.x + tl.x * x, tl.y + tl.y * y,);
+    }
+}
