6894e8334e92820e929a9ca81c6ea16d82ee5200
EvilAlliance <chialanza17@gmail.com>
2024-02-16 19:09:31 -0300

    All function inside class Eraser

--- src/WhiteBoard/Cursor/Eraser.ts
+++ src/WhiteBoard/Cursor/Eraser.ts
@@ -1,8 +1,9 @@
-import { Canvas, CanvasClear, CanvasRender } from '../Canvas';
+import { Canvas } from '../Canvas';
 import { arrIdentical } from '../CommonMethod';
 import { Point } from '../GeoSpace/Point';
-import { BaseObject, BaseObjectCanvasData, CtxSetting } from '../Object/BaseObject';
+import { BaseObject } from '../Object/BaseObject';
 import { CanvasObjectContainer } from '../Object/CanvasObjectContainer';
+import { CtxSetting } from '../Object/CtxSetting';
 import { Path } from '../Object/Path';
 import { BaseBrush } from './BaseBrush';
 import { EraserAllObjectCeaseExist } from './EraserAll';
@@ -22,7 +23,55 @@ export class Eraser extends BaseBrush<WeakMap<BaseObject, Path>>{
     }
 
     mouseMove(canvas: Canvas<this>, e: MouseEvent, obj: WeakMap<BaseObject, Path>): void {
-        EraserMouseMove(canvas, e, obj);
+        const mousePoint = new Point(e.offsetX, e.offsetY);
+        for (const object of canvas.Objects) {
+            if (!object.render) return;
+            if (object.Object.pointInRange(mousePoint, canvas.cursor.width, canvas.cursor.tolerance)) {
+                canvas.cursor.activeWorker.delete(object);
+                const path = obj.get(object.Object);
+                if (!path) {
+                    //const beforeData = BaseObjectCanvasData(object.Object);
+                    const p = new Path({
+                        ctxSetting: new CtxSetting({
+                            strokeWidth: canvas.cursor.width,
+                            lineCap: canvas.cursor.lineCap,
+                            globalCompositeOperation: 'destination-out',
+                        }),
+                        Path: [mousePoint],
+                    });
+
+                    object.Object.erased.push(p);
+                    obj.set(object.Object, p);
+
+                    //const afterData = BaseObjectCanvasData(object.Object);
+
+                    /*
+                    if (!arrIdentical(beforeData, afterData)) {
+                        obj.set(object.Object, p);
+                    } else {
+                        object.Object.erased.pop();
+                    }
+                    */
+                } else {
+                    //const beforeData = BaseObjectCanvasData(object.Object);
+                    path.Path.push(mousePoint);
+                    //const afterData = BaseObjectCanvasData(object.Object);
+                    /*
+                    if (arrIdentical(beforeData, afterData)) {
+                        console.log('removed')
+                        path.Path.pop();
+                        obj.delete(object.Object);
+                    }
+                    */
+                }
+                canvas.clear();
+                canvas.render();
+            } else {
+                if (obj.delete(object.Object)) {
+                    canvas.cursor.activeWorker.set(object, EraserAllObjectCeaseExist(canvas, object));
+                }
+            }
+        }
     }
 
     mouseUp(canvas: Canvas<this>, e: MouseEvent, obj: WeakMap<BaseObject, Path>): void {
@@ -33,56 +82,3 @@ export class Eraser extends BaseBrush<WeakMap<BaseObject, Path>>{
         }
     }
 }
-
-export function EraserMouseMove(canvas: Canvas<Eraser>, e: MouseEvent, obj: WeakMap<BaseObject, Path>) {
-    const mousePoint = new Point(e.offsetX, e.offsetY);
-    for (const object of canvas.Objects) {
-        if (!object.render) return;
-        if (object.Object.pointInRange(object.Object, mousePoint, canvas.cursor.width, canvas.cursor.tolerance)) {
-            canvas.cursor.activeWorker.delete(object);
-            const path = obj.get(object.Object);
-            if (!path) {
-                //const beforeData = BaseObjectCanvasData(object.Object);
-                const p = new Path({
-                    ctxSetting: new CtxSetting({
-                        strokeWidth: canvas.cursor.width,
-                        lineCap: canvas.cursor.lineCap,
-                        globalCompositeOperation: 'destination-out',
-                    }),
-                    Path: [mousePoint],
-                });
-
-                object.Object.erased.push(p);
-                obj.set(object.Object, p);
-
-                //const afterData = BaseObjectCanvasData(object.Object);
-
-                /*
-                if (!arrIdentical(beforeData, afterData)) {
-                    obj.set(object.Object, p);
-                } else {
-                    object.Object.erased.pop();
-                }
-                */
-            } else {
-                //const beforeData = BaseObjectCanvasData(object.Object);
-                path.Path.push(mousePoint);
-                //const afterData = BaseObjectCanvasData(object.Object);
-                /*
-                if (arrIdentical(beforeData, afterData)) {
-                    console.log('removed')
-                    path.Path.pop();
-                    obj.delete(object.Object);
-                }
-                */
-            }
-            CanvasClear(canvas);
-            CanvasRender(canvas);
-        } else {
-            if (obj.delete(object.Object)) {
-                canvas.cursor.activeWorker.set(object, EraserAllObjectCeaseExist(canvas, object));
-            }
-        }
-    }
-}
-
