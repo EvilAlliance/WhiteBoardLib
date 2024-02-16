7ccb9c26d2b81353723cece583d5e0e7c63462c1
EvilAlliance <chialanza17@gmail.com>
2024-02-16 19:45:48 -0300

    Change JSON to a Map

--- src/WhiteBoard/Observable.ts
+++ src/WhiteBoard/Observable.ts
@@ -1,11 +1,11 @@
 export type TEventCallback<T = any> = (options: T) => void;
 
 export class Observable<T = Object>{
-    eventListener: Record<keyof T, TEventCallback[]> = {} as Record<keyof T, TEventCallback[]>;
+    eventListener: Map<keyof T, TEventCallback[]> = new Map<keyof T, TEventCallback[]>();
 
     on<K extends keyof T = keyof T>(evtName: K, handler: TEventCallback<T[K]>): VoidFunction {
-        if (!this.eventListener[evtName]) this.eventListener[evtName] = [];
-        this.eventListener[evtName].push(handler);
+        if (!this.eventListener.has(evtName)) this.eventListener.set(evtName, []);
+        (this.eventListener.get(evtName) as TEventCallback[]).push(handler);
         return () => this.off(evtName, handler);
     }
 
@@ -18,9 +18,10 @@ export class Observable<T = Object>{
     }
     removeEventListener<K extends keyof T>(eventName: K, handler?: TEventCallback<T[K]>) {
         if (!handler) {
-            this.eventListener[eventName] = [];
+            this.eventListener.set(eventName, []);
         } else {
-            const eventListener = this.eventListener[eventName];
+            const eventListener = this.eventListener.get(eventName);
+            if (!eventListener) return;
             const index = eventListener.indexOf(handler);
             index > -1 && eventListener.splice(index, 1);
         }
@@ -28,7 +29,7 @@ export class Observable<T = Object>{
 
     off<K extends keyof T>(arg0: K, handler: TEventCallback<T[K]>) {
         if (!arg0) {
-            for (const eventName in this.eventListener) {
+            for (const eventName of this.eventListener.keys()) {
                 this.removeEventListener(eventName);
             }
         } else {
@@ -37,7 +38,7 @@ export class Observable<T = Object>{
     }
 
     fire<K extends keyof T>(eventName: K, options: T[K]) {
-        const events = this.eventListener[eventName] || [];
+        const events = this.eventListener.get(eventName) || [];
         for (let i = 0; i < events.length; i++) {
             events[i].call(this, options || {});
         }
