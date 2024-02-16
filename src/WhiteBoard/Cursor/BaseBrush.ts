56fe43570ff0f7a5f5f99bb56ac620993cd46b57
EvilAlliance <chialanza17@gmail.com>
2024-02-16 19:04:47 -0300

    Beatifying BaseBrush

--- src/WhiteBoard/Cursor/BaseBrush.ts
+++ src/WhiteBoard/Cursor/BaseBrush.ts
@@ -1,25 +1,24 @@
-import { Canvas, CanvasEvents } from '../Canvas';
-import { on } from '../Observable';
+import { Canvas } from '../Canvas';
 
 export abstract class BaseBrush<T = void> {
     width: number = 10;
+
+    init(canvas: Canvas<typeof this>, e: MouseEvent) {
+        const temp = canvas.cursor.mouseDown(canvas);
+
+        canvas.cursor.mouseMove(canvas, e, temp);
+
+        const x = canvas.on('mouse:move', function(this: Canvas, e: MouseEvent) {
+            canvas.cursor.mouseMove(canvas, e, temp);
+        })
+
+        canvas.once('mouse:up', function(this: Canvas, e: MouseEvent) {
+            x();
+            canvas.cursor.mouseUp(canvas, e, temp);
+        });
+    }
+
     abstract mouseDown(canvas: Canvas<typeof this>): T;
     abstract mouseMove(canvas: Canvas<typeof this>, e: MouseEvent, obj: T): void;
     abstract mouseUp(canvas: Canvas<typeof this>, e: MouseEvent, obj: T): void;
-}
-
-export function BaseBrushMouseDown(canvas: Canvas, e: MouseEvent) {
-    const temp = canvas.cursor.mouseDown(canvas);
-
-    canvas.cursor.mouseMove(canvas, e, temp);
-
-    const x = on<CanvasEvents, 'mouse:move'>(canvas, 'mouse:move', function(this: Canvas, e: MouseEvent) {
-        canvas.cursor.mouseMove(canvas, e, temp);
-    });
-
-    const y = on<CanvasEvents, 'mouse:up'>(canvas, 'mouse:up', function(this: Canvas, e: MouseEvent) {
-        x();
-        y();
-        canvas.cursor.mouseUp(canvas, e, temp);
-    });
 }
