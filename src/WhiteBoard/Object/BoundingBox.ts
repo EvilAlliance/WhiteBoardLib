f753cb4c2ebe3b0d50d063d79bc117e1e116032c
EvilAlliance <chialanza17@gmail.com>
2024-02-16 15:53:38 -0300

    BoundingBox with their respective function

---
+++ src/WhiteBoard/Object/BoundingBox.ts
@@ -0,0 +1,31 @@
+import { Point } from "../GeoSpace/Point";
+
+export class BoundingBox {
+    tl: Point;
+    tr: Point;
+    bl: Point;
+    br: Point;
+
+    constructor(tl: Point, tr: Point, bl: Point, br: Point) {
+        this.tl = tl;
+        this.tr = tr;
+        this.bl = bl;
+        this.br = br;
+    }
+
+    tranform(transformationMat: DOMMatrix) {
+        const tl = new DOMPointReadOnly(this.tl.x, this.tl.y).matrixTransform(transformationMat);
+        const tr = new DOMPointReadOnly(this.tr.x, this.tr.y).matrixTransform(transformationMat);
+        const bl = new DOMPointReadOnly(this.bl.x, this.bl.y).matrixTransform(transformationMat);
+        const br = new DOMPointReadOnly(this.br.x, this.br.y).matrixTransform(transformationMat);
+
+        this.tl.x = tl.x;
+        this.tl.y = tl.y;
+        this.tr.x = tr.x;
+        this.tr.y = tr.y;
+        this.bl.x = bl.x;
+        this.bl.y = bl.y;
+        this.br.x = br.x;
+        this.br.y = br.y;
+    }
+}
