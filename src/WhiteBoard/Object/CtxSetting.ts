0ac4f06ba89853c2c298f58a238c40f9ee5d08bd
EvilAlliance <chialanza17@gmail.com>
2024-02-16 15:52:52 -0300

    CtxSetting Class with their respective function

---
+++ src/WhiteBoard/Object/CtxSetting.ts
@@ -0,0 +1,23 @@
+import { CanvasParseColor, Color } from "../Utils/Color";
+
+export class CtxSetting {
+    lineCap: CanvasLineCap = 'round';
+    strokeWidth: number = 0;
+    strokeColor: Color = 'Red';
+    fill: boolean = false;
+    fillColor: Color = 'Red';
+    globalCompositeOperation: GlobalCompositeOperation = 'source-over';
+    constructor(obj?: Partial<CtxSetting>) {
+        Object.assign(this, obj);
+    }
+
+    setSettingSetContextOption(ctx: CanvasRenderingContext2D) {
+        ctx.lineCap = this.lineCap;
+        ctx.lineWidth = this.strokeWidth;
+        ctx.strokeStyle = CanvasParseColor(this.strokeColor);
+
+        ctx.fillStyle = CanvasParseColor(this.fillColor);
+
+        ctx.globalCompositeOperation = this.globalCompositeOperation;
+    }
+}
